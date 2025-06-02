7c2a0ed04fd510b7008e8c136de030f8047b4c84
yves1-357 <152566364+yves1-357@users.noreply.github.com>
2025-06-02 15:44:03 +0200

    petite modif

--- routes/api.php
+++ routes/api.php
@@ -4,8 +4,6 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ChatController;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+
 
 Route::post('/chat', [ChatController::class, 'ask']);
