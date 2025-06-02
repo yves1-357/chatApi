6ac4fd1432dfe24333a25ec9f767787b2f8291de
yves1-357 <152566364+yves1-357@users.noreply.github.com>
2025-06-02 12:13:36 +0200

    Supprime les routes inutiles

--- routes/web.php
+++ routes/web.php
@@ -4,21 +4,6 @@ use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
-Route::get('/', function () {
-    return Inertia::render('Welcome', [
-        'canLogin' => Route::has('login'),
-        'canRegister' => Route::has('register'),
-        'laravelVersion' => Application::VERSION,
-        'phpVersion' => PHP_VERSION,
-    ]);
-});
-
-Route::middleware([
-    'auth:sanctum',
-    config('jetstream.auth_session'),
-    'verified',
-])->group(function () {
-    Route::get('/dashboard', function () {
-        return Inertia::render('Dashboard');
-    })->name('dashboard');
+Route::get('/chat', function(){
+    return Inertia::render('Chat');
 });
