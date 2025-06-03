5bf744b0190bee8e3b89f0e7d5013b21bc614d2b
yves1-357 <152566364+yves1-357@users.noreply.github.com>
2025-06-03 20:43:24 +0200

    ajout des routes

--- routes/api.php
+++ routes/api.php
@@ -3,7 +3,12 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ChatController;
+use App\Http\Controllers\ConversationController;
+use App\Http\Controllers\MessageController;
 
 
 
 Route::post('/chat', [ChatController::class, 'ask']);
+Route::get('/conversations', [ConversationController::class, 'index']);
+Route::get('/conversations/{id}/messages', [MessageController::class, 'index']);
+Route::delete('/conversations', [ConversationController::class, 'destroyAll']);
