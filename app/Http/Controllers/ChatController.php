30b51d0f429a23a91b799b0e833e02a63370aa01
yves1-357 <152566364+yves1-357@users.noreply.github.com>
2025-06-03 20:36:47 +0200

    cree conversation/mets api en pause

--- app/Http/Controllers/ChatController.php
+++ app/Http/Controllers/ChatController.php
@@ -4,14 +4,40 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Http;
+use App\Models\Conversation;
+use App\Models\Message;
 
 class ChatController extends Controller
 {
     public function ask(Request $request){
         $question = $request->input('question');
-        //app openrouter
+        $conversationId = $request->input('conversation_id');
+
+        //on cree une nouvel conversation si pas  conversation_id
+
+        if(!$conversationId){
+            $title = substr($question, 0, 40); // titre::1ere caractere du question
+            $conversation = Conversation::create(['title' => $title]);
+        } else {
+            $conversation = Conversation::find($conversationId);
+        }
+
+        //enregistrer msg vide
+        if(empty($question)){
+            $question ="message vide";
+        }
+
+        //enregistrer msg user
+        Message::create([
+            'conversation_id' => $conversation->id,
+            'role' => 'user',
+            'content' =>  $question,
+        ]);
+
+        //app openrouter à activer plus tard ======
+        /*
         $reponse = Http::withHeaders([
-            'Authorization' => 'Bearer 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',
+            'Authorization' => 'Bearer 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',
             'HTTP-Referer' => 'https://localhost',
             'OpenAI-Referer' => 'https://localhost',
         ])->post('https://openrouter.ai/api/v1/chat/completions',[
@@ -29,8 +55,26 @@ class ChatController extends Controller
         } else {
             $answer = "Erreur Api :" . $reponse->body();
         }
+        */
+
+        $answer = "Réponse automatique stella.";
+
+        //:jamais enregistrer un msg vide(securite)
+        if (empty($answer)) {
+            $answer = "Pas de réponse.";
+        }
+
+        //enregistrer reponse ia
+         Message::create([
+            'conversation_id' => $conversation->id,
+            'role' => 'assistant',
+            'content' => $answer,
+        ]);
+
+
         return response()->json([
             'answer' => $answer,
+            'conversation_id' => $conversation->id,
         ]);
 
     }
