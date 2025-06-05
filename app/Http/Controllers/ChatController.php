35556c269106f1546ca4e906e23e61a475fb1744
yves1-357 <152566364+yves1-357@users.noreply.github.com>
2025-06-05 18:59:06 +0200

    petite modif a changer plutard

--- app/Http/Controllers/ChatController.php
+++ app/Http/Controllers/ChatController.php
@@ -35,14 +35,21 @@ class ChatController extends Controller
         ]);
 
         //app openrouter à activer plus tard ======
-        /*
+
         $reponse = Http::withHeaders([
-            'Authorization' => 'Bearer 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',
+            'Authorization' => 'Bearer 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',
             'HTTP-Referer' => 'https://localhost',
             'OpenAI-Referer' => 'https://localhost',
         ])->post('https://openrouter.ai/api/v1/chat/completions',[
             'model' =>'openai/gpt-3.5-turbo', //modele simple pour debuter
             'messages' => [
+                // instruction llm a voir plutard
+                [
+                    'role' => 'system',
+                    'content' => "Tu es Stella, un assistant amical et concis. réponds clairement en evitant les fautes. utilise un ton poli et écoute bien la question."
+                ],
+
+                //message user
                 ['role' => 'user', 'content' => $question]
             ],
             'max_tokens' => 500,
@@ -55,8 +62,9 @@ class ChatController extends Controller
         } else {
             $answer = "Erreur Api :" . $reponse->body();
         }
-        */
 
+
+        /*
         $answer = "Réponse automatique stella.";
 
         //:jamais enregistrer un msg vide(securite)
@@ -64,6 +72,7 @@ class ChatController extends Controller
             $answer = "Pas de réponse.";
         }
 
+        */
         //enregistrer reponse ia
          Message::create([
             'conversation_id' => $conversation->id,
