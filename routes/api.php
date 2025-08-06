2b3970182864bed65ae2d9c58f7caeead7ac27ed
yves1-357 <152566364+yves1-357@users.noreply.github.com>
2025-08-06 03:10:38 +0200

    modif sur les routes

--- routes/api.php
+++ routes/api.php
@@ -5,10 +5,14 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ChatController;
 use App\Http\Controllers\ConversationController;
 use App\Http\Controllers\MessageController;
+use App\Http\Controllers\AuthController;
 
-
+// echange de données par exemple front-end pose question / conversations.etc..
 
 Route::post('/chat', [ChatController::class, 'ask']);
 Route::get('/conversations', [ConversationController::class, 'index']);
 Route::get('/conversations/{id}/messages', [MessageController::class, 'index']);
 Route::delete('/conversations', [ConversationController::class, 'destroyAll']);
+Route::post('register', [AuthController::class, 'register']);
+Route::post('login',    [AuthController::class, 'login']);
+
--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,34 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
+use App\Http\Controllers\AuthController;
+
+
+// Page « guest chat »
+Route::get('/', function() {
+    return Inertia::render('GuestChat');
+});
+
+
+// affichage princiapal via inertia
 
 Route::get('/chat', function(){
     return Inertia::render('Chat');
 });
+
+
+// Page d'inscription
+Route::get('/register', function () {
+    return Inertia::render('Register');
+});
+
+// Page de connexion
+Route::get('/login', function () {
+    return Inertia::render('Login');
+});
+
+// Traitement form-inscription
+Route::post('/register', [AuthController::class, 'register']);
+
+// Traitement form-login
+Route::post('/login',    [AuthController::class, 'login']);
