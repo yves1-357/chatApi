0fa7b18a992998f885f396a6f3f2dc232dfacfa8
yves1-357 <152566364+yves1-357@users.noreply.github.com>
2025-08-07 02:25:48 +0200

    quelques modifs

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,6 +1,6 @@
 import './bootstrap';
 import '../css/app.css';
-
+import axios from 'axios'
 import { createApp, h } from 'vue';
 import { createInertiaApp } from '@inertiajs/vue3';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,4 +1,15 @@
 import axios from 'axios';
+axios.defaults.baseURL = '/';
+axios.defaults.withCredentials = true;
+
+// récupère automatiquement le token CSRF inséré par Laravel
+const token = document.head.querySelector('meta[name="csrf-token"]');
+if (token) {
+  axios.defaults.headers.common['X-CSRF-TOKEN'] = token.content;
+}
+
+axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 window.axios = axios;
 
-window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+
+
